95fcc79f238147acc73a3207598ddafc210190af
AlexTheGreatRO <alexthegreatro@gmail.com>
2022-04-18 21:39:54 +0300

    Register Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    firstname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    lastname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.max(130),
+      Validators.min(6),
+    ]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  get username() {
+    return this.registerForm.get('username') as FormControl;
   }
 
+  get email() {
+    return this.registerForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registerForm.get('password') as FormControl;
+  }
+
+  get firstname() {
+    return this.registerForm.get('firstname') as FormControl;
+  }
+
+  get lastname() {
+    return this.registerForm.get('lastname') as FormControl;
+  }
+
+  get age() {
+    return this.registerForm.get('age') as FormControl;
+  }
+
+  register() {
+    console.log(this.username.value);
+  }
+
+  ngOnInit(): void {}
 }
