590202314ccfb9d1e7a40de6dae0a2e2fbdf39a2
AlexTheGreatRO <alexthegreatro@gmail.com>
2022-05-09 21:45:03 +0300

    Added log out

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,64 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FirestoreService } from './firestore.service';
+import { UserProfile } from './data/user.profile';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    public afauth: AngularFireAuth,
+    public firestoreService: FirestoreService
+  ) {
+    afauth.authState.subscribe(async (user) => {
+      if (user) {
+        const data = await firestoreService.getUser(user.email!);
+        const profile = data.docs[0].data();
+
+        const email = profile['email'];
+        const age = profile['age'];
+        const firstname = profile['firstname'];
+        const lastname = profile['lastname'];
+        const username = profile['username'];
+
+        const profileData = new UserProfile(
+          age,
+          email,
+          firstname,
+          lastname,
+          username
+        );
+        localStorage.setItem('currentUser', JSON.stringify(profileData));
+      } else {
+        console.log('User has been logged out!');
+      }
+    });
+  }
+
   logIn(email: string, password: string) {
     return this.afauth.signInWithEmailAndPassword(email, password);
   }
-  constructor(public afauth: AngularFireAuth) {}
 
   signUp(email: string, password: string) {
     return this.afauth.createUserWithEmailAndPassword(email, password);
+  }
+
+  logOut() {
+    localStorage.removeItem('currentUser');
+    return this.afauth.signOut();
+  }
+
+  getCurrentUser(): UserProfile | undefined {
+    const profileString = localStorage.getItem('currentUser');
+    if (!profileString) return undefined;
+    const profile = JSON.parse(profileString);
+    return new UserProfile(
+      profile['age'],
+      profile['email'],
+      profile['firstname'],
+      profile['lastname'],
+      profile['username']
+    );
   }
 }
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { UserProfile } from 'src/app/data/user.profile';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
+  logOut() {
+    this.authservice.logOut();
+    this.userProfile = undefined;
+  }
+  userProfile?: UserProfile;
 
-  constructor() { }
+  constructor(private authservice: AuthService) {}
 
   ngOnInit(): void {
+    this.userProfile = this.authservice.getCurrentUser();
   }
-
 }
